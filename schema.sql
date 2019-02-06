d545f05d6a4721a9ec69c1b28153435185a2b8d2
Mayja Tom <maja.morales17@gmail.com>
2019-02-06 14:56:09 -0800

    updating schema name

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
 -- Drops the sparrow_db if it exists currently --
-DROP DATABASE IF EXISTS sparrow_2.0;
+DROP DATABASE IF EXISTS sparrow_2;
 -- Creates the "sparrow_db" database --
-CREATE DATABASE sparrow_2.0;
+CREATE DATABASE sparrow_2;
